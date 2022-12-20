cbfd1f795e120129abbb16d46d6442dd55ad52ab
Rafael Cabral <72042885+rafaelcabral223@users.noreply.github.com>
2022-12-20 19:49:38 -0300

    Add files via upload

--- CarMania/main.js
+++ CarMania/main.js
@@ -9,3 +9,15 @@ let header = document.querySelector('header');
 window.addEventListener('scroll' , () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
+
+var btnContainer = document.getElementById("nav-container");
+
+var btns = btnContainer.getElementsByClassName("btn-nav");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
